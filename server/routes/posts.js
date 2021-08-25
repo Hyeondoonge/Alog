4fcaeb2c7b1bda0eaba4625456ea9ef7b89e0448
Hyeondoonge <migu554@naver.com>
2021-08-25 23:59:54 +0900

    update 특정 작성자의 포스트를 대상하는 검색 기능

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,14 +7,19 @@ const router = express.Router();
 // data formatting 함수 만들기
 
 router.get('/search', async (req, res, next) => {
-
   const totalCount = await countPosts();
-  const posts = await findPost(req.query.keyword);
+  const { keyword, writerId } = req.query;
+  const posts = await findPost(keyword, writerId);
 
-  res.status(200).json({
-    totalCount,
-    posts
-  });
+  if (!posts.length) {
+    res.status(204).end();
+  }
+  else {
+    res.status(200).json({
+      totalCount,
+      posts
+    });
+  }
 });
 
 export default router;
--- server/utils/post.js
+++ server/utils/post.js
@@ -2,8 +2,6 @@ import Post from '../models/Post.js';
 
 // 연산 작성법 => 1. save() 2. query / 각각을 사용했을 때 차이점이 있다 (casting / middleware / validation)
 
-// const Post = model('Post', postSchema);
-
 const insertPosts = async (data) => {
   Post.insertMany(data, (err) => {
     if (err) console.log(err);
@@ -12,19 +10,19 @@ const insertPosts = async (data) => {
 };
 
 // find
-const findPost = async (keyword) => {
+const findPost = async (keyword, writerId) => {
    // json > query builder, query builder 사용 시 체인이 많이 필요할 경우 가독성이 떨어진다
   try {
-    if (keyword) {
-      const doc = await Post.find({ title: { $regex: '.*' + keyword + '.*' }}).exec();
-      return doc;
-    } else {
-      return [];
-    }
+    if (!keyword) return [];
+    const filter = { title: { $regex: '.*' + keyword + '.*' } };
+    if (writerId) filter.writerId = writerId;
+
+    const doc = await Post.find(filter).exec();
+    return doc;
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 const countPosts = async () => {
  try {
