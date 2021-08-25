d768960756b72d40d6d20e371a4ac3f2009b78e8
Hyeondoonge <migu554@naver.com>
2021-08-26 00:09:19 +0900

    add Alog 로고

---
+++ client/src/common/Logo.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default function Logo() {
+  return (
+    <div
+      style={{
+        textAlign: 'center'
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '50px',
+          color: '#5F939A'
+        }}
+      >
+        ALOG
+      </h1>
+    </div>
+  );
+}
