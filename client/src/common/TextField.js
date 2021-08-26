e112418911ed55fdfc7ad8a7c3fb5db697cd01a3
Hyeondoonge <migu554@naver.com>
2021-08-26 23:49:36 +0900

    add input 컴포넌트

---
+++ client/src/common/TextField.js
@@ -0,0 +1,49 @@
+import { useContext, useRef } from 'react';
+import styled from 'styled-components';
+import ThemeContext from '../contexts/ThemeContext';
+
+// input?
+// 사용자 정보 입력할 때도 쓰일 듯
+
+const StyledTextFieldWrapper = styled.div`
+  width: fit-content;
+  border: 1.5px solid ${(props) => props.color};
+  border-radius: 25px;
+  box-shadow: 0 0 3px 1px ${(props) => props.color};
+  padding: 2% 3%;
+  font-size: 3rem;
+`;
+
+const StyledTextField = styled.input`
+  font-size: inherit;
+  border: 0px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default function TextField({ size, placeholder, handleChange, handleKeyUp }) {
+  const inputRef = useRef(null);
+  const theme = useContext(ThemeContext);
+
+  return (
+    <StyledTextFieldWrapper color={theme.main}>
+      <StyledTextField
+        ref={inputRef}
+        size={size}
+        type="text"
+        placeholder={placeholder}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+      />
+      <span
+        style={{ cursor: 'pointer' }}
+        onClick={() => {
+          inputRef.current.value = '';
+        }}
+      >
+        ✖️
+      </span>
+    </StyledTextFieldWrapper>
+  );
+}
