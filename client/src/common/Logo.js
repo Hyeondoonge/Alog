7e517bf64c2418e94776a021f61ba1db0910bbda
Hyeondoonge <migu554@naver.com>
2021-08-27 23:27:03 +0900

    Add 다크모드와 로고에 링크 추가

--- client/src/common/Logo.js
+++ client/src/common/Logo.js
@@ -1,20 +1,18 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function Logo() {
   return (
-    <div
-      style={{
-        textAlign: 'center'
-      }}
-    >
+    <Link to="/" style={{ textDecoration: 'none' }}>
       <h1
         style={{
-          fontSize: '50px',
-          color: '#5F939A'
+          fontSize: '3.5rem',
+          color: '#5F939A',
+          textAlign: 'center'
         }}
       >
         ALOG
       </h1>
-    </div>
+    </Link>
   );
 }
