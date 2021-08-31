0326e84396d643f1c32546ef851450a0f312441f
Hyeondoonge <migu554@naver.com>
2021-08-31 22:58:29 +0900

    fix usePostHook의 useEffect promise 반환 -> 일반 함수 반환

--- client/src/post/Search.js
+++ client/src/post/Search.js
@@ -1,17 +1,17 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { debounce } from 'lodash';
 import { searchPost } from './fetchApis';
 import TextField from '../common/TextField';
 import List from '../common/List';
 import Post from './Post';
 import ThemeContext from '../contexts/ThemeContext';
+import Loading from '../common/Loading';
 
 // 백엔드로 빼기
 const languages = [
   'C++',
   'java',
   'C',
-  'Node.js',
   'MySQL',
   'javascript',
   'Oracle',
@@ -22,14 +22,17 @@ const languages = [
   'Swift'
 ];
 
+const PostList = ({ filteredPosts }) =>
+  filteredPosts.map(({ _id: id, ...post }) => <Post key={id} post={post} />);
+
 const usePostHook = () => {
+  const [isLoading, setIsLoading] = useState(0);
   const [posts, setPosts] = useState([]);
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState(null);
   const [selectedFilter, setSelectedFilter] = useState({
     'C++': false,
     java: false,
     C: false,
-    'Node.js': false,
     MySQL: false,
     javascript: false,
     Oracle: false,
@@ -40,20 +43,28 @@ const usePostHook = () => {
     Swift: false
   });
 
-  useEffect(async () => {
-    // use async closure?
-    const res = await searchPost(keyword);
-    const newPosts = res?.posts ?? [];
-    setPosts(newPosts);
+  const fetchPosts = async (keyword) => {
+    setIsLoading(1);
+
+    if (!keyword) {
+      setPosts([]);
+    } else {
+      const res = await searchPost(keyword);
+      setPosts(res?.posts ?? []);
+    }
+    setIsLoading(0);
+  };
+
+  useEffect(() => {
+    fetchPosts(keyword);
   }, [keyword]);
 
-  return [posts, setKeyword, selectedFilter, setSelectedFilter];
+  return [isLoading, posts, keyword, setKeyword, selectedFilter, setSelectedFilter];
 };
 
 export default function Search() {
   // 필터 토글 시 포스트도 변경되야함, hooks따로 만들기
-  const textRef = useRef(null);
-  const [posts, setKeyword, selectedFilter, setSelectedFilter] = usePostHook();
+  const [isLoading, posts, keyword, setKeyword, selectedFilter, setSelectedFilter] = usePostHook();
   let filteredPosts = [];
 
   if (languages.some((language) => selectedFilter[language] === true)) {
@@ -63,14 +74,14 @@ export default function Search() {
   }
   const theme = useContext(ThemeContext);
 
-  const handleChange = (event) => {
-    if (textRef) textRef.current.innerHTML = `you're typing...`;
+  const handleKeyUp = async (event) => {
+    // 왜 async..?
+    const keyword = event.target.value;
+    setKeyword(keyword);
   };
 
-  const handleKeyUp = async (event) => {
-    const keyword = event.target.value;
-    textRef.current.innerHTML = `search ${keyword}`;
-    setKeyword(keyword);
+  const handleClickRemove = () => {
+    setKeyword(null);
   };
 
   return (
@@ -78,37 +89,32 @@ export default function Search() {
       style={{
         display: 'flex',
         flexDirection: 'column',
-        gap: 5,
+        gap: 20,
         justifyContent: 'center'
       }}
     >
       <TextField
         size={50}
         placeholder="찾고자하는 문제의 제목을 입력하세요."
-        handleChange={handleChange}
         handleKeyUp={debounce(handleKeyUp, 500)}
+        handleClickRemove={handleClickRemove}
       />
-      <span ref={textRef}>.</span>
       <div>
         <List elements={languages} states={selectedFilter} updateStates={setSelectedFilter} />
       </div>
-      <span style={{ fontSize: '1rem' }}>
-        {filteredPosts?.length
-          ? `검색 결과 ${filteredPosts.length}개의 풀이`
-          : '검색 결과가 없습니다.'}
-      </span>
-      {filteredPosts && (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '2rem'
-          }}
-        >
-          {filteredPosts.map((post) => (
-            <Post post={post} />
-          ))}
-        </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {keyword && (
+            <span style={{ fontSize: '1.5rem' }}>
+              {filteredPosts?.length
+                ? `검색 결과 ${filteredPosts.length}개의 풀이`
+                : '검색 결과가 없습니다.'}
+            </span>
+          )}
+          {filteredPosts && <PostList filteredPosts={filteredPosts} />}
+        </>
       )}
     </div>
   );
