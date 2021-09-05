c4b06465aff4c5959b3e38312b1c6f0a2979896f
Hyeondoonge <migu554@naver.com>
2021-09-05 10:04:56 +0900

    Add 포스트 리스트 무한 스크롤

--- client/src/post/Search.js
+++ client/src/post/Search.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { debounce } from 'lodash';
 import { searchPost } from './fetchApis';
 import TextField from '../common/TextField';
@@ -23,11 +23,13 @@ const languages = [
 ];
 
 const PostList = ({ filteredPosts }) =>
-  filteredPosts.map(({ _id: id, ...post }) => <Post key={id} post={post} />);
+  filteredPosts.map(({ _id: id, ...post }) => <Post key={id} post={{ id, ...post }} />);
 
 const usePostHook = () => {
   const [isLoading, setIsLoading] = useState(0);
   const [posts, setPosts] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [intersecting, setIntersecting] = useState(0);
   const [keyword, setKeyword] = useState(null);
   const [selectedFilter, setSelectedFilter] = useState({
     'C++': false,
@@ -42,47 +44,126 @@ const usePostHook = () => {
     'C#': false,
     Swift: false
   });
+  const size = 10; // 10개씩 fetch
 
-  const fetchPosts = async (keyword) => {
+  const fetchPostsByKeyword = async (keyword) => {
     setIsLoading(1);
 
     if (!keyword) {
+      setTotalCount(0);
       setPosts([]);
+      setIsLoading(0);
     } else {
-      const res = await searchPost(keyword);
-      setPosts(res?.posts ?? []);
+      const res = await searchPost(keyword, size);
+      setTotalCount(res?.totalCount ?? 0);
+      setPosts([...(res?.posts ?? [])]);
+      setIsLoading(0);
     }
-    setIsLoading(0);
+  };
+
+  const fetchPostsByCursor = async () => {
+    setIsLoading(1);
+
+    if (!keyword) {
+      setTotalCount(0);
+      setPosts([]);
+      setIsLoading(0);
+    } else {
+      const { length } = posts;
+      const cursor = posts[length - 1]._id; // 맨 마지막 요소의 아이디 = 커서
+
+      const res = await searchPost(keyword, size, cursor);
+
+      setTotalCount(res?.totalCount ?? 0);
+      setPosts([...posts, ...(res?.posts ?? [])]);
+      setIsLoading(0);
+    }
+    setIntersecting(0);
   };
 
   useEffect(() => {
-    fetchPosts(keyword);
+    fetchPostsByKeyword(keyword);
   }, [keyword]);
 
-  return [isLoading, posts, keyword, setKeyword, selectedFilter, setSelectedFilter];
+  useEffect(() => {
+    // 조건문 달아주지 않으면, intersecting이 0일 때도 요청이 일어난다.
+    if (intersecting) fetchPostsByCursor();
+  }, [intersecting]);
+
+  return [
+    isLoading,
+    posts,
+    totalCount,
+    keyword,
+    setKeyword,
+    selectedFilter,
+    setSelectedFilter,
+    setIntersecting
+  ];
 };
 
 export default function Search() {
   // 필터 토글 시 포스트도 변경되야함, hooks따로 만들기
-  const [isLoading, posts, keyword, setKeyword, selectedFilter, setSelectedFilter] = usePostHook();
+  const [
+    isLoading,
+    posts,
+    totalCount,
+    keyword,
+    setKeyword,
+    selectedFilter,
+    setSelectedFilter,
+    setIntersecting
+  ] = usePostHook();
+
+  const postRef = useRef(null);
+  const observer = useRef(null);
+
   let filteredPosts = [];
 
-  if (languages.some((language) => selectedFilter[language] === true)) {
-    filteredPosts = posts.filter(({ language }) => selectedFilter[language] === true);
+  if (languages.some((language) => selectedFilter[language])) {
+    filteredPosts = posts.filter(({ language }) => selectedFilter[language]);
   } else {
     filteredPosts = posts;
   }
+
   const theme = useContext(ThemeContext);
 
   const handleKeyUp = async (event) => {
-    // 왜 async..?
-    const keyword = event.target.value;
-    setKeyword(keyword);
+    setKeyword(event.target.value);
   };
 
   const handleClickRemove = () => {
     setKeyword(null);
   };
+
+  useEffect(() => {
+    const { length } = posts;
+
+    if (!length) return;
+    if (totalCount === length) return;
+
+    const target = postRef.current.children[length - 3];
+
+    if (observer.current != null) observer.current.disconnect(); // 새로운 옵저버 생성 전 정리
+    observer.current = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIntersecting(1); // 교차했으니 커서 변경이벤트 발생
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 1
+      }
+    );
+    observer.current.observe(target);
+
+    return () => {
+      if (observer.current != null) observer.current.disconnect();
+    };
+  }, [posts]);
 
   return (
     <div
@@ -99,23 +180,16 @@ export default function Search() {
         handleKeyUp={debounce(handleKeyUp, 500)}
         handleClickRemove={handleClickRemove}
       />
-      <div>
-        <List elements={languages} states={selectedFilter} updateStates={setSelectedFilter} />
-      </div>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          {keyword && (
-            <span style={{ fontSize: '1.5rem' }}>
-              {filteredPosts?.length
-                ? `검색 결과 ${filteredPosts.length}개의 풀이`
-                : '검색 결과가 없습니다.'}
-            </span>
-          )}
-          {filteredPosts && <PostList filteredPosts={filteredPosts} />}
-        </>
+      <List elements={languages} states={selectedFilter} updateStates={setSelectedFilter} />
+      {keyword && (
+        <span style={{ fontSize: '2rem' }}>
+          {totalCount ? `검색 결과 ${totalCount}개의 풀이` : '검색 결과가 없습니다.'}
+        </span>
       )}
+      <div ref={postRef} style={{ display: 'flex', flexDirection: 'column', gap: 'inherit' }}>
+        {filteredPosts && <PostList filteredPosts={filteredPosts} />}
+      </div>
+      {isLoading !== 0 && <Loading />}
     </div>
   );
 }
--- client/src/post/fetchApis.js
+++ client/src/post/fetchApis.js
@@ -1,10 +1,14 @@
-const searchPost = async (keyword) => {
+const searchPost = async (keyword, size, cursor) => {
   try {
-    const response = await fetch(`/posts/search?keyword=${keyword}`, {
-      // proxy
-      method: 'get'
-    });
-    if (response.status === 204) return null;
+    const response = await fetch(
+      `/posts/search?keyword=${keyword}&cursor=${cursor ?? 'none'}&size=${size}`,
+      {
+        // proxy
+        method: 'get'
+      }
+    );
+
+    if (response.status === 204) return;
     const result = await response.json();
     return result;
   } catch (err) {
