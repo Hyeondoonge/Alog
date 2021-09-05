b10a41bee04e546fd83dff1e8edad238cfcc4141
Hyeondoonge <migu554@naver.com>
2021-09-05 09:58:23 +0900

    Update 커서로 포스트 조회 / 포스트 날짜 포맷 변경

--- server/utils.js
+++ server/utils.js
@@ -8,20 +8,20 @@ const formatDate = (date) => {
   const writeDate = moment(date);
 
   if (today.diff(writeDate, 'years')) {
-    return writeDate.format('YYYY년 MM월 DD일 H:mm');
+    return writeDate.format('YYYY년 MM월');
   }
    
   if (today.diff(writeDate, 'months') || today.diff(writeDate, 'days')) {
-    return writeDate.format('MM월 DD일 H:mm');
+    return writeDate.format('MM월 DD일');
   }
 
-  return writeDate.format('H:mm');
+  return `오늘 ${writeDate.format('H:mm')}`;
 };
 
 /**
  * doc 타입의 요소들에 대해 값을 조작
- * @param {*} docs Mongoose Document 타입 배열
- * @param {*} callback 각 요소에 수행할 함수 
+ * @param {Array} docs Mongoose Document 타입 배열
+ * @param {function} callback 각 요소에 수행할 함수 
  * @returns 각 요소에 수행한 callback 결과를 모은 새ㅗㄹ운 배열
  */
 const docsMap = (docs, callback) => (
