300133bcacd6d3a4b8cdccaa948b3a766bbf295c
Hyeondoonge <migu554@naver.com>
2021-11-11 23:31:29 +0900

    Update 풀이 등록 요청 시 (context에서 얻은)사용자의 아이디와 함께 요청

--- client/src/page/WritePost.js
+++ client/src/page/WritePost.js
@@ -1,27 +1,29 @@
 import { useContext, useState } from 'react';
 import ThemeContext from '../contexts/ThemeContext';
+import ModalContext from '../contexts/ModalContext';
+import UserContext from '../contexts/UserContext';
 import Form from '../common/Form';
 import Button from '../common/Button';
 import Template from '../Template';
 import { fetchSolution_POST } from '../form/fetchApis';
-import ModalContext from '../contexts/ModalContext';
 
 export default function WritePost() {
   const theme = useContext(ThemeContext);
   const [setMessage] = useContext(ModalContext);
+  const [_, ___, userData] = useContext(UserContext);
+
   const [post, setPost] = useState({
     title: '',
     platform: '',
     subtitle: '',
     language: '',
-    content: '',
-    writerId: 'jsi06138'
+    content: ''
   });
 
   const onClick = () => {
     // 데이터 유효성 검사
     (async () => {
-      const res = await fetchSolution_POST(post);
+      const res = await fetchSolution_POST(post, userData.userId);
       const json = await res.json();
       if (res.status === 201) {
         window.location.href = `/post?id=${json.post._id}`;
