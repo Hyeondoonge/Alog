bb653d53ca940082308684eb663798781286c46d
Hyeondoonge <migu554@naver.com>
2021-11-15 18:18:07 +0900

    Add 인가가 필요한 곳에서 사용 가능한 useAuthorization 훅 생성
    - refresh token 유효기간 7일에서 무한으로 변경
    - 자동로그인 로직 UserContext에서 App의 MyComonent 내부로 이동

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,51 @@ import ReadPost from './page/ReadPost';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import { ThemeContextProvider } from './contexts/ThemeContext';
 import { ModalContextProvider } from './contexts/ModalContext';
-import { UserContextProvider } from './contexts/UserContext';
+import UserContext, { UserContextProvider } from './contexts/UserContext';
 import SignUp from './page/SignUp';
+import { useContext, useEffect } from 'react';
+import useAuthorization from './hooks/useAuthorizaiton';
+import { fetchAutoSignin_GET } from './api/authApi';
+
+const MyComponent = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn, userData, setUserData] = useContext(UserContext);
+  const authorization = useAuthorization();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const accessToken = await authorization();
+        if (!accessToken) return;
+        const res = await fetchAutoSignin_GET(accessToken);
+        const { userId, msg } = await res.json();
+        setUserData({ ...userData, userId: userId });
+        setIsLoggedIn(true);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return <>{children}</>;
+};
 
 export default function App() {
   return (
     <ThemeContextProvider>
       <ModalContextProvider>
         <UserContextProvider>
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/write" exact component={WritePost} />
-              <Route path="/edit" exact component={EditPost} />
-              <Route path="/post" exact component={ReadPost} />
-              <Route path="/signup" exact component={SignUp} />
-              {/* edit/:postsId */}
-            </Switch>
-          </Router>
+          <MyComponent>
+            <Router>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/write" exact component={WritePost} />
+                <Route path="/edit" exact component={EditPost} />
+                <Route path="/post" exact component={ReadPost} />
+                <Route path="/signup" exact component={SignUp} />
+                {/* edit/:postsId */}
+              </Switch>
+            </Router>
+          </MyComponent>
         </UserContextProvider>
       </ModalContextProvider>
     </ThemeContextProvider>
