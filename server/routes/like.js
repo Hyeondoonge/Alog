abc072803bea78fedab7dfdc26b2b99d6c748c0e
Hyeondoonge <migu554@naver.com>
2021-11-18 22:53:57 +0900

    Update 브라우저 밖에서 날라올 수 있는 http 요청에 대한 방어

--- server/routes/like.js
+++ server/routes/like.js
@@ -1,35 +1,42 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { deleteLike, createLike, findLiker } from '../queries/like.js';
 const router = express.Router();
 
+router.use((req, res, next) => {
+  try {
+    const accessToken = req.headers['authorization'].split(' ')[1]; 
+    jwt.verify(accessToken, process.env.ACCESS_SECRET_KEY);
+    next();
+  } catch (error) {
+    res.status(401).end();
+  }
+});
+
+// 현재 body는 쉽게 조작이 가능하다.
 router.post('/', async (req, res, next) => {
   try {
-    console.log(req.body);
-    const { postId, userId } = req.body;
-    await createLike(req.body);
+    const { userId, postId } = req.body;
+
+    await createLike(postId, userId);
     const liker = await findLiker(postId);
     const isLiker = liker.some(({ userId: id }) => userId === id);
 
-    setTimeout(() => {
-      res.json({ likeCount: liker.length, isLiker });
-    }, 3000);
+    res.json({ likeCount: liker.length, isLiker });
   } catch (error) {
-    console.log(error);
     res.json({ msg: '실패!' });
   }
 });
 
 router.delete('/', async (req, res, next) => {
   try {
-    console.log(req.body);
-    const { postId, userId } = req.body;
-    await deleteLike(req.body);
+    const { userId, postId } = req.body;
+
+    await deleteLike(postId, userId);
     const liker = await findLiker(postId);
     const isLiker = liker.some(({ userId: id }) => userId === id);
 
-    setTimeout(() => {
-      res.json({ likeCount: liker.length, isLiker });
-    }, 3000);
+    res.json({ likeCount: liker.length, isLiker });
   } catch (error) {
     res.json({ msg: '실패!' });
   }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { insertPost, updatePost, findPost, findPosts, countPosts, leftPosts } from '../queries/post.js';
 import { findLanguage } from '../queries/language.js';
-import { docsMap, formatDate } from '../utils.js';
+import {  formatDate } from '../utils.js';
 import { findLiker } from '../queries/like.js';
 const router = express.Router();
 
@@ -12,6 +13,46 @@ router.get('/', async (req, res) => {
   const post = await findPost(id);
   res.status(200).json(post);
 });
+
+// posts search
+// writerId, language, cursor는 undefined로 전달될 수도 있는 값.
+// 클라이언트 단에서 부터 요청 전 이를 처리해주도록 해야한다.
+router.get('/search', async (req, res) => {
+  // keyword uri encode 하개ㅣ
+
+  let { keyword, languages, cursor, size, writerId } = req.query;
+  keyword = keyword.replace(/\+/g, '\\+');
+
+  const totalCount = await countPosts(keyword, languages ? languages.split(',') : null, writerId);
+  const leftCount = await leftPosts(keyword, languages ? languages.split(',') : null, cursor, writerId);
+  let posts = await findPosts(keyword, languages ? languages.split(',') : null, cursor, size, writerId);
+  // doc 데이터들 변형
+  posts = posts.map((post) => post.toObject());
+
+  for(const post of posts) {
+    post.likeCount = (await findLiker(post._id)).length;
+    post.writeDate = formatDate(post.writeDate);
+  }
+
+  res.status(200).json({
+      totalCount,
+      leftCount: leftCount - posts.length,
+      posts,
+    });
+});
+
+// 인가
+router.use((req, res, next) => {
+  try {
+    const accessToken = req.headers['authorization'].split(' ')[1]; 
+    jwt.verify(accessToken, process.env.ACCESS_SECRET_KEY);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).end();
+  }
+});
+
 
 router.post('/', async (req, res) => {
   const { title, platform, language, content } = req.body;
@@ -50,33 +91,6 @@ router.put('/', async (req, res) => {
   }
   const post = await updatePost(id, req.body);
   res.status(201).json({ post, msg: '정상적으로 글이 수정됐습니다' });
-});
-
-// posts search
-// writerId, language, cursor는 undefined로 전달될 수도 있는 값.
-// 클라이언트 단에서 부터 요청 전 이를 처리해주도록 해야한다.
-router.get('/search', async (req, res) => {
-  // keyword uri encode 하개ㅣ
-
-  let { keyword, languages, cursor, size, writerId } = req.query;
-  keyword = keyword.replace(/\+/g, '\\+');
-
-  const totalCount = await countPosts(keyword, languages ? languages.split(',') : null, writerId);
-  const leftCount = await leftPosts(keyword, languages ? languages.split(',') : null, cursor, writerId);
-  let posts = await findPosts(keyword, languages ? languages.split(',') : null, cursor, size, writerId);
-  // doc 데이터들 변형
-  posts = posts.map((post) => post.toObject());
-
-  for(const post of posts) {
-    post.likeCount = (await findLiker(post._id)).length;
-    post.writeDate = formatDate(post.writeDate);
-  }
-
-  res.status(200).json({
-      totalCount,
-      leftCount: leftCount - posts.length,
-      posts,
-    });
 });
 
 export default router;
