37fde08bc4526f1b8758aab316daec59e6750862
Hyeondoonge <migu554@naver.com>
2022-01-05 00:25:19 +0900

    Fix 언어 필터 렌더링하도록 조건문 변경

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -139,7 +139,7 @@ export default function Home() {
           }
         />
         <div>
-          {isLanguageLoading && (
+          {!isLanguageLoading && (
             <FilterList
               elements={languages}
               state={isSelected}
