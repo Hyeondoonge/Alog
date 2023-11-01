3eebdef8cef8355f88dfdec326a35948dac28ba1
Hyeondoonge <migu554@naver.com>
2023-11-01 14:18:56 +0900

    Test lazy 코드 제거

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ import useToken from './hooks/useToken';
 import UserHome from './page/UserHome';
 import jwtDecode from 'jwt-decode';
 import Loading from './common/Loading';
-const WritePost = lazy(() => import('./page/WritePost'));
-const EditPost = lazy(() => import('./page/EditPost'));
+import WritePost from './page/WritePost';
+import EditPost from './page/EditPost';
 
 const MyComponent = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn, userData, setUserData] = useContext(UserContext);
