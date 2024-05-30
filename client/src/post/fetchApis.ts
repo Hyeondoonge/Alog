2eb3dfa5f1a0b27c9d13b6928c8601c0708bcfa1
Hyeondoonge <migu554@naver.com>
2024-05-30 22:42:33 +0900

    refactor: main 페이지 관련 API ts 변환

--- client/src/post/fetchApis.ts
+++ client/src/post/fetchApis.ts
@@ -1,20 +1,35 @@
+import { Language, Option } from 'types/api';
+import { IPost } from 'types/post';
+
 // createQuery를 만들기 위해 값이 비거나 없는 속성은 제거하고
 // url query를 생성
-const createQuery = (option) => {
-  return Object.keys(option)
-    .filter((key) => option[key].length !== 0 && option[key])
-    .map((key) => {
-      let value = option[key];
-      if (key === 'languages') value = encodeURIComponent(option[key]); // 인코딩 대상 query: langauge
-      return `${key}=${value}`;
+const createQuery = (option: Option) => {
+  Object.keys(option)
+    .filter((key) => {
+      const typedKey = key as keyof Option;
+      const value = option[typedKey];
+
+      if (!value) return false; // undefined, null, 0, '' 우선 제거
+
+      if (value instanceof Array) {
+        return value.length !== 0;
+      }
+
+      return true;
     })
-    .join('&');
+    .map((key) => {
+      const typedKey = key as keyof Option;
+      let value = option[typedKey];
+      if (key === 'languages') value = encodeURIComponent(JSON.stringify(value)); // 인코딩 대상 query: langauge
+      return `${key}=${value}`;
+    });
 };
 
-const fetchLanguages_GET = async () => {
+// response type이 정상적일 때의 반환값으로 충분?
+const fetchLanguages_GET: () => Promise<{ languages: Language[] } | undefined> = async () => {
   try {
     const response = await fetch('/api/languages');
-
+    // TODO: 안전한 에러 핸들링 추가
     const result = await response.json();
     return result;
   } catch (err) {
@@ -34,7 +49,11 @@ const fetchPost_GET = async (id) => {
   }
 };
 
-const fetchPosts_GET = async (option) => {
+const fetchPosts_GET: (
+  option: Option
+) => Promise<{ posts: IPost[]; totalCount: number; leftCount: number } | undefined> = async (
+  option
+) => {
   try {
     const query = createQuery(option);
     const response = await fetch(`/api/posts/search?${query}`);
