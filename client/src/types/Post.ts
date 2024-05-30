efbae926787e648e6197d0c67ff75c26bbe2adea
Hyeondoonge <migu554@naver.com>
2024-05-30 22:46:23 +0900

    refactor: Post 타입 수정

--- client/src/types/Post.ts
+++ client/src/types/Post.ts
@@ -1,8 +1,8 @@
 export interface IPost {
   _id: string;
   title: string;
-  subtitle: string;
-  language: string;
+  subtitle?: string;
+  language?: string;
   writerId: string;
   writeDate: string;
   likeCount: number;
