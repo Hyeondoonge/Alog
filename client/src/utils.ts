e6a6b57025965828da0d60cb4fdb2d50fbafc17a
Hyeondoonge <migu554@naver.com>
2024-07-15 22:27:23 +0900

    refactor: option query string 관리 로직 분리

---
+++ client/src/utils.ts
@@ -0,0 +1,36 @@
+const KEYWORD_KEY = 'keyword';
+const FILTER_KEY = 'filter';
+
+export const OptionQueryString = {
+  createQSUsingKeyword: (keyword: string) => {
+    const { languages: original_languages } = OptionQueryString.getOption();
+    const urlSearchParams = new URLSearchParams();
+
+    if (keyword) {
+      urlSearchParams.append(KEYWORD_KEY, keyword);
+    }
+    urlSearchParams.append(FILTER_KEY, encodeURIComponent(JSON.stringify(original_languages)));
+
+    return urlSearchParams;
+  },
+  createQSUsingSelectedLanguages: (selectedLanguages: string[]) => {
+    const { keyword: original_keyword } = OptionQueryString.getOption();
+    const urlSearchParams = new URLSearchParams();
+
+    if (original_keyword) {
+      urlSearchParams.append(KEYWORD_KEY, original_keyword);
+    }
+    urlSearchParams.append(FILTER_KEY, encodeURIComponent(JSON.stringify(selectedLanguages)));
+
+    return urlSearchParams;
+  },
+  getOption: () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const keywordParam = urlSearchParams.get(KEYWORD_KEY) || '';
+    const filterParam: null | string[] = JSON.parse(
+      decodeURIComponent(urlSearchParams.get(FILTER_KEY) || 'null')
+    );
+
+    return { keyword: keywordParam, languages: filterParam };
+  }
+};
