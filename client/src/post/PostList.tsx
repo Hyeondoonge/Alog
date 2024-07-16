b33f1076162c6fbdaf359105b1a640fbaf11b2eb
Hyeondoonge <migu554@naver.com>
2024-07-16 15:37:24 +0900

    refactor: post(풀이) 상태 관리 로직을 PostList 컴포넌트로 분리

--- client/src/post/PostList.tsx
+++ client/src/post/PostList.tsx
@@ -1,9 +1,12 @@
-import { IPost } from 'types/post';
 import Post from './Post';
 import styled from 'styled-components';
 import { useEffect, useRef } from 'react';
 import useIntersectionObserver from 'hooks/useIntersectionObserver';
 import Skeleton from 'common/Skeleton';
+import useOptionStore from 'store/option';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchPosts_GET } from './fetchApis';
+import useLanguagesStore from 'store/languages';
 
 const StyledPostList = styled.div`
   display: flex;
@@ -11,17 +14,67 @@ const StyledPostList = styled.div`
   gap: inherit;
 `;
 
-interface PostListProps {
-  isLoading: boolean;
-  posts: IPost[];
-  handleIntersect: () => void;
-}
+// TODO: Home, UserHome 재사용성 향상
+export default function PostList() {
+  const size = 50;
+  const { keyword, isSelected } = useOptionStore((state) => ({
+    keyword: state.keyword,
+    isSelected: state.isSelected
+  }));
+  const { languages } = useLanguagesStore((state) => ({
+    languages: state.languages
+  }));
+  const filteredLanguages = languages
+    .filter((_, index) => isSelected[index])
+    .map(({ name }) => name);
 
-export default function PostList({ isLoading, posts, handleIntersect }: PostListProps) {
+  const metadata = useRef<{ totalCount: null | number; leftCount: null | number }>({
+    totalCount: 0,
+    leftCount: 0
+  });
+  const { totalCount, leftCount } = metadata.current;
+
+  const { data, fetchNextPage, isFetchingNextPage, error } = useInfiniteQuery({
+    queryKey: ['posts', keyword, filteredLanguages],
+    queryFn: async ({ pageParam }) => {
+      if (!keyword) {
+        metadata.current = { totalCount: 0, leftCount: 0 };
+        return [];
+      }
+
+      const res = await fetchPosts_GET({
+        keyword,
+        languages: filteredLanguages,
+        cursor: pageParam,
+        size
+      });
+
+      if (!res) {
+        throw new Error('failed to fetch post');
+      }
+      const { posts, totalCount, leftCount } = res;
+      metadata.current = { totalCount, leftCount };
+      return posts;
+    },
+    initialPageParam: '',
+    getNextPageParam: (lastPage) => (lastPage.length ? lastPage[lastPage.length - 1]._id : null),
+    throwOnError: false
+  });
+
   const ref = useRef<HTMLDivElement | null>(null);
   const { createObserver, registerTargets } = useIntersectionObserver();
 
+  const handleIntersect = () => {
+    if (leftCount === 0) return;
+    fetchNextPage();
+  };
+
   useEffect(() => {
+    if (data === undefined) return;
+    const { pages } = data;
+
+    const posts = pages.flat();
+
     if (posts.length === 0) return;
     if (ref.current === null) {
       return;
@@ -32,16 +85,33 @@ export default function PostList({ isLoading, posts, handleIntersect }: PostList
     }
     createObserver(handleIntersect);
     registerTargets([lastPost]);
-  }, [posts]);
+  }, [data]);
+
+  if (error || !data) {
+    // FIX: Error boundary 없는 관계로 임의 코드 삽입
+    return <div>Error</div>;
+  }
+
+  const posts = data.pages.flat();
 
   return (
     <>
+      {keyword && (!isFetchingNextPage || posts.length !== 0) && (
+        <span style={{ fontSize: '2rem' }}>
+          {totalCount ? `검색 결과 ${totalCount}개의 풀이` : '검색 결과가 없습니다.'}
+        </span>
+      )}
+      {isFetchingNextPage && !posts.length && (
+        <Skeleton
+          Component={<div style={{ width: '20rem', height: '5rem', borderRadius: '2rem' }} />}
+        />
+      )}
       <StyledPostList ref={ref}>
         {posts.map((post) => (
           <Post key={post._id} post={post} />
         ))}
       </StyledPostList>
-      {isLoading && (
+      {isFetchingNextPage && (
         <div style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: 50 }}>
           {new Array(3).fill(null).map((_, index) => (
             <div
